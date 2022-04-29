4c01dd68132d9190bafeb9030056e044f522251c
byxhp <byxhp@hotmail.com>
2022-04-29 09:03:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tradutor_Instantâneo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
